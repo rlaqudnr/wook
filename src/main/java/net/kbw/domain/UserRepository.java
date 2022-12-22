f0a1c87181d7705d23ad0571eb68e3c3e83b0615
DESKTOP-N0BUQNT\HOME <rlaqudnr@naver.com>
2022-12-23 01:42:42 +0900

    ㅇㄴ

--- src/main/java/net/kbw/domain/UserRepository.java
+++ src/main/java/net/kbw/domain/UserRepository.java
@@ -1,8 +1,13 @@
 package net.kbw.domain;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long>{
+
+	User findByUserId(String UserId);
+	
 
 	
 
--- src/main/java/net/kbw/wook/UserController.java
+++ src/main/java/net/kbw/wook/UserController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,6 +26,12 @@ public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	
+	
+	
+	
+	
 	
 	@GetMapping("/form")
 	public String form() {
@@ -49,14 +57,40 @@ public class UserController {
 		
 	}
 	
-	@GetMapping("/login")
-	public String login() {
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		
+		
+		
+		
 		
 		return "/user/login";
 		
 		
 		
 	}
+	
+	@PostMapping("/login")
+	public String login(String userId,String password ,HttpSession session) {
+		
+		User user =userRepository.findByUserId(userId);
+		
+		if(user == null) {
+			return "redirect:/users/loginForm";
+			
+		}
+		if(password.equals(user.getPassword())) {
+			
+			return "redirect:/users/loginForm";
+		}
+		
+		session.setAttribute("user", user);
+		return "redirect:/";
+		
+		
+		
+	}
+	
 	
 	
 	
