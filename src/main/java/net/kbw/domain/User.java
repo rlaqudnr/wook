d48671a8e81d66c96de70d1d42811a6781fbb238
DESKTOP-N0BUQNT\HOME <rlaqudnr@naver.com>
2023-01-16 05:12:30 +0900

    ㅎㅎㅎ

--- src/main/java/net/kbw/domain/User.java
+++ src/main/java/net/kbw/domain/User.java
@@ -73,10 +73,8 @@ public class User {
 	public String getUserId() {
 		return userId;
 	}
-	@Override
-	public String toString() {
-		return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
-	}
+	
+	
 	public String getPassword() {
 		return password;
 	}
