a66a10dd9d66d53d08ebd31cb127fdef911e7def
DESKTOP-N0BUQNT\HOME <rlaqudnr@naver.com>
2023-01-16 07:31:09 +0900

    ㅋㅋ

--- src/test/java/com/example/demo/WookApplicationTests.java
+++ src/test/java/com/example/demo/WookApplicationTests.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootConfiguration
+
 
 @SpringBootTest
 class WookApplicationTests {
